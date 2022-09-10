7cd254d2766d86e6721afb306f5687daa3163c44
Andrew Ho <andyminhtuanho@gmail.com>
2022-09-10 11:29:24 -0700

    Added psycopg2 to dependencies

--- app/config.py
+++ app/config.py
@@ -1,9 +1,6 @@
 
 import os
 
-# DB_NAME = os.environ.get("DB_NAME")
-DB_NAME = "database.db"
-
 # CONFIG
 SECRET_KEY = os.environ.get("SECRET_KEY")
 SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
